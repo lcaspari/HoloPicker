9f766c48fbdcddaefe5a75dc63accdd65919c4bb
lcaspari <72619797+lcaspari@users.noreply.github.com>
2021-09-19 19:51:30 +0200

    added textpackage and tried ViewList
    nothing works, tomorrow continue

--- HoloPicker_Unity/Assets/Scripts/ViewList.cs
+++ HoloPicker_Unity/Assets/Scripts/ViewList.cs
@@ -4,11 +4,39 @@ using UnityEngine;
 
 public class ViewList : MonoBehaviour
 {
+
+    public GameObject gridObjectCollection; 
     public InventoryManager inventoryManager;
+    public GameObject text;
+    private GameObject track;
     // Start is called before the first frame update
+    
     void Start()
     {
+        // Get the order item list from inventory manager
+        List<InventoryManager.OrderItem> items = new List<InventoryManager.OrderItem>();
+        items = inventoryManager.order.orderItem;
+        // Create a list of strings from the order items
         List<string> attributes = new List<string>();
+        Debug.Log("ANZAHL ITEMS: " + items.Count);
+        foreach (InventoryManager.OrderItem item in items)
+        {
+            attributes.Add(item.order);
+            attributes.Add(item.quantity.ToString());
+            attributes.Add(item.id);
+            attributes.Add(item.name);
+            attributes.Add(item.category);
+            attributes.Add(item.location.ToString());
+        }
+
+        foreach (string attribute in attributes)
+        {
+            track = Instantiate(text, transform);
+            track.GetComponentInChildren<UnityEngine.UI.Text>().text = attribute;
+            Debug.Log("NAME: " + track.GetComponentInChildren<UnityEngine.UI.Text>().text);
+        }
+
+ 
     }
 
     // Update is called once per frame
